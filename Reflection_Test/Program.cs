4a0dee8136b7c48b67033a3078be415c9d896d29
JustMe17Artem <90451087+JustMe17Artem@users.noreply.github.com>
2022-02-13 20:17:56 +0300

    iskan chekai

---
+++ Reflection_Test/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Reflection;
+
+
+namespace Reflection_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string dllName = @"C:\Users\ИМЕННО ОН\source\repos\LEISURECore\LEISURECore\bin\Debug\LEISURECore";
+            Assembly asm = Assembly.LoadFrom(dllName);
+            Type type = asm.GetType("LEISURECore.DataAccess", true, true);
+            Console.WriteLine($"Type: {type.AssemblyQualifiedName}");
+            
+            Console.WriteLine($"Members:");
+            foreach (MemberInfo memberInfo in type.GetMembers())
+            {
+                Console.WriteLine($"    {memberInfo.DeclaringType} {memberInfo.MemberType} {memberInfo.Name}");
+               
+            }
+            Console.WriteLine($"Methods:");
+            foreach (MethodInfo method in type.GetMethods())
+            {
+                Console.Write($"    {method.ReturnType} {method.Name} (");
+                foreach (ParameterInfo param in method.GetParameters())
+                {
+                    Console.Write($"{param.ParameterType.Name} {param.Name}, ");
+                }
+                Console.WriteLine(")");
+            }
+
+        }
+
+    }
+    
+}
