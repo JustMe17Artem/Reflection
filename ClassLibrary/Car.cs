3abeb84661d60461478a1f61ec6714feb65a47ae
JustMe17Artem <sematkinartem6@gmail.com>
2022-02-24 22:51:39 +0300

    app for reflection

---
+++ ClassLibrary/Car.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ClassLibrary
+{
+    public class Car
+    {
+        public string Name { get; set; }
+        public int Speed { get; set; }
+        public int Power { get; set; }
+        public int HoldingPeriod { get; set; }
+
+        public Car(string name, int speed, int power, int holdingPeriod)
+        {
+            Name = name;
+            Speed = speed;
+            Power = power;
+            HoldingPeriod = holdingPeriod;
+        }
+        public Car()
+        {
+            
+        }
+        public void StartEngine()
+        {
+            Console.WriteLine("Вжух, двигатель заведён");
+        }
+        public void Drive()
+        {
+            StartEngine();
+            Console.WriteLine($"Машина {Name} едет со скоростью {Speed} км/ч");
+        }
+        public double TaxCalculation(int rate)
+        {
+            return rate*Power*(HoldingPeriod/12);
+        }
+    }
+}
