61acf8387a8a897df8bdf9326738c8cf1640e3f6
JustMe17Artem <sematkinartem6@gmail.com>
2022-02-24 22:41:59 +0300

    full reflection

--- Reflection_Test/Program.cs
+++ Reflection_Test/Program.cs
@@ -2,15 +2,16 @@
 using System.Reflection;
 
 
+
 namespace Reflection_Test
 {
     class Program
     {
         static void Main(string[] args)
         {
-            string dllName = @"C:\Users\ИМЕННО ОН\source\repos\LEISURECore\LEISURECore\bin\Debug\LEISURECore";
+            string dllName = @"C:\Users\ИМЕННО ОН\source\repos\ReflectionProject\ClassLibrary\bin\Debug\netcoreapp3.1\ClassLibrary";
             Assembly asm = Assembly.LoadFrom(dllName);
-            Type type = asm.GetType("LEISURECore.DataAccess", true, true);
+            Type type = asm.GetType("ClassLibrary.Car", true, true);
             Console.WriteLine($"Type: {type.AssemblyQualifiedName}");
             
             Console.WriteLine($"Members:");
@@ -29,7 +30,25 @@ namespace Reflection_Test
                 }
                 Console.WriteLine(")");
             }
+            object car = Activator.CreateInstance(type, "Volvo", 150, 100, 12);
 
+            MethodInfo methodDrive = type.GetMethod("Drive");
+            methodDrive.Invoke(car, new object[] { });
+
+            MethodInfo methodTaxCalc = type.GetMethod("TaxCalculation");
+            double sum = (double)methodTaxCalc.Invoke(car, new object[] {12});
+            Console.WriteLine("Сумма транспортного налога:" + sum + "руб");
+
+
+            ConstructorInfo carConstructor = type.GetConstructor(new Type[] {  });
+            var newCar = carConstructor.Invoke(new object[] { });
+            Console.WriteLine("Констурктор  " + ((carConstructor == null) ? "не найден" : "найден"));
+            var propertySpeed = type.GetProperty("Speed");
+            var propertyName= type.GetProperty("Name");
+            propertyName.SetValue(newCar, "BMW");
+            propertySpeed.SetValue(newCar, 111);
+            Console.WriteLine(propertyName.GetValue(newCar));
+            Console.WriteLine(propertySpeed.GetValue(newCar));
         }
 
     }
---
+++ Triangle/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Threading;
+ 
+namespace Triangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Write("Введите 1 сторону:");
+                double side1 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите 2 сторону:");
+                double side2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Введите 3 сторону:");
+                double side3 = Convert.ToDouble(Console.ReadLine());
+                Triangle triangle = new Triangle(side1, side2, side3);
+                if (!triangle.TriangleIsValid())
+                {
+                    Console.WriteLine("Такой треугольник не может существовать");
+                }
+                else
+                {
+                    Console.WriteLine(triangle.TriangleTypeDetermination());
+                    Console.WriteLine(triangle.TypeAngleDetermination());
+                    Console.WriteLine("Площадь треугольника: " + triangle.FindTriangleSquare());
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Неверный формат ввода");
+            }
+            
+        }
+
+        
+    }
+    class Triangle
+    {
+        public bool TriangleIsValid()
+        {
+            if ((side1 == 0 || side2 == 0 || side3 == 0) || (side1 < 0 || side2 < 0 || side3 < 0)
+               ||(side1+side2) <= side3 || (side1 + side3) <= side2 || (side2 + side3) <= side1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+        public Triangle(double s1, double s2, double s3)
+        {
+            side1 = s1;
+            side2 = s2;
+            side3 = s3;
+        }
+        public double side1 { get; set; }
+        public double side2 { get; set; }
+        public double side3 { get; set; }
+
+
+        public string TriangleTypeDetermination()
+        {
+            if ((side1 == side2) && (side2 == side3))
+            {
+                return "Треугольник равносторонний";
+            }
+            else if(((side1 == side2) && (side2 != side3)) 
+                   ||((side1 == side3) && (side2 != side3)) 
+                   ||((side2 == side3) && (side1 != side3)))
+            {
+                return "Треугольник равнобедреный";
+            }
+            else
+            {
+                return "Треугольник разносторонний";
+            }
+        }
+        private static void Sort(double a, double b, double c, out double mx, out double sr, out double mn)
+        {
+            mx = a;
+            if (b > mx) mx = b;
+            if (c > mx) mx = c;
+
+            mn = a;
+            if (b < mn) mn = b;
+            if (c < mn) mn = c;
+            sr = a + b + c - mx - mn;
+        }
+        public string TypeAngleDetermination()
+        {
+            double d = 0, e = 0, max, srd, min;
+            Sort(side1, side2, side3, out max, out min, out srd);
+            d = min * min + srd * srd;
+            e = max * max;
+            if (d > e) return "Остроугольный треугольник";
+            else if (d < e) return "Тупоугольный треугольник";
+            else return "Прямоугольный треугольник";
+        }
+        public double FindTriangleSquare()
+        {
+            double p = (side1 + side2 + side3) / 2;
+            return Math.Sqrt(p * (p - side1) * (p - side2) * (p - side3));
+        }
+
+    }
+
+}
